7a65d9ccfd826f9a209d264a27eb28b7ad9c3fd2
z4ha <jakmusze@gmail.com>
2017-10-25 23:17:24 +0200

    Add 2 letters limiter and result view

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var text = '';
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -10,6 +11,15 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    if (msg != 'stop' && msg.length <= 2) {
+    text = text + msg
+    console.log('text => ',text);
+    console.log('msg => ',msg);
+    }
+    if (msg === "stop") {
+      io.emit('result', text);
+      text = '';
+    }
   });
 });
 
